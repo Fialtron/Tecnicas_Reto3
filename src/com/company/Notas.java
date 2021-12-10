c285fce662c630c4c7c618dc57ad4d8eefb506ac
Juan Moreno <moreno.juandi73@gmail.com>
2021-12-10 13:48:43 -0500

    Final punto 1

--- src/com/company/Notas.java
+++ src/com/company/Notas.java
@@ -19,19 +19,22 @@ public class Notas {
                 System.out.println("\nDigite las de notas que desea evaluar separadas cada una por una coma ( 3,4,2 ) y en el rango de 0 a 5" +
                         "\nNo deje espacios entre las notas, no inserte numeros negativos, no inserte carcteres diferente a numeros");
                 cadena= input.next();
+                System.out.println(cadena);
                 notes = new float[cadena.split(",").length];
+
                 for (int i = 0; i < cadena.split(",").length; i++) {
                     notes[i] = Float.parseFloat(cadena.split(",")[i]);
-                    if (!(notes[i] <= 5 && notes[i] >= 0)) {
+                    if (notes[i] > 5 || notes[i] < 0 ) {
                         System.out.println("Inserte un numero mayor a cero y menor a 5. Por favor intente nuevamente\n");
-                        i -= 1;
+                        return;
                     }
                 }
                 ordenamiento();
+                System.out.println("La mayor nota es: "+notes[notes.length-1] +"\n");
+                System.out.println("La menor nota es: "+ notes[0]+"\n");
+                System.out.println("El promedio es: "+ promedio() +"\n");
+                System.out.println("La desviacion estandar es: "+ desviacion() +"\n");
 
-                for (int i = 0; i < notes.length; i++) {
-                    System.out.println("\n"+notes[i]);
-                }
 
 
                 exit = false;
@@ -44,28 +47,37 @@ public class Notas {
     }
 
     private void ordenamiento(){
-        int i = 0;
-        float auxiliar = 0;
-        while (i < notes.length ){
-            for (int j = 0; j < notes.length - i; j++) {
-                if(notes[j] > notes[j+1]){
-                    auxiliar = notes[j];
-                    notes[j] = notes[j+1];
-                    notes[j+1] = auxiliar;
+        for (int x = 0; x < notes.length; x++) {
+            for (int y = 0; y < notes.length - 1; y++) {
+                float elementoActual = notes[y],
+                        elementoSiguiente = notes[y + 1];
+                if (elementoActual > elementoSiguiente) {
+
+                    notes[y] = elementoSiguiente;
+                    notes[y + 1] = elementoActual;
                 }
             }
-            i++;
         }
+
     }
 
+    private float promedio(){
+        float prom = 0;
+        for (int i = 0; i < notes.length; i++) {
+            prom += notes[i];
+        }
+        return prom/notes.length;
+    }
 
-/*
-
-
-
- */
-
-
-
-
+    private float desviacion(){
+        float des = 0;
+        float prom = promedio();
+        for (int i = 0; i < notes.length; i++) {
+            des+= Math.pow(notes[i] - prom, 2);
+        }
+        des = des/notes.length;
+        des =(float) Math.sqrt(des) ;
+        return des;
+    }
+    
 }
